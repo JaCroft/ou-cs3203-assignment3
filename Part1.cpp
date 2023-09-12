82da087614f54ddbc0701b031825956ac4fa87a3
Jason Hollcroft <Jason.A.Hollcroft-1@ou.edu>
2023-09-12 00:00:23 -0500

    started part1 of assignment

--- Part1.cpp
+++ Part1.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <array>
 
+using namespace std;
+
+int addArray(int x[]);
+
+int addArray(int x[])
+{
+	int sum = 0;
+	for (int i = 0; i < x.size() - 1; i++)
+	{
+		sum += x[i];
+	}
+}
